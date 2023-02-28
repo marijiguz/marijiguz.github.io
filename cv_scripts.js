72735f97994fd93ccb84adbb712af44b89df8f40
marijiguz <mjguz78@gmail.com>
2023-02-28 18:46:02 -0300

    28 6:45

--- cv_scripts.js
+++ cv_scripts.js
@@ -1,67 +1,43 @@
 
 $(document).ready(main);
-
-var contador = 1;
-
-function main(){
-	$('.menu').click(function(){
-		// $('nav').toggle(); 
-
-		if(contador == 1){
-			$('nav').animate({
-				left: '0'
-			});
-			contador = 0;
-		} else {
-			contador = 1;
-			$('nav').animate({
-				left: '-100%'
-			});
-		}
-
-	});
-
+var contador = 0;
+function main() {
+    $('.menu').click(function(){
+	// $('nav').toggle(); 
+        contador = (contador + 1)%2
+        if(contador == 1){
+            $('nav').animate({
+		
+                left: '0'
+            });
+            
+        } else {
+            
+            $('nav').animate({
+                left: '-100%'
+            });
+        }
+    });
 };
 
 
-function vermas(id){
-switch (id) {
-  case "mas":
-  document.getElementById("desplegar").style.display="block"; 
-  document.getElementById("mas").style.display="none";
-  break;
-  case "mas3":
-  document.getElementById("desplegar3").style.display="block"; 
-  document.getElementById("mas3").style.display="none";
-  break;
-  case "mas2":
-  document.getElementById("desplegar2").style.display="block"; 
-  document.getElementById("mas2").style.display="none";
-  break;
-  case "mas1":
-  document.getElementById("desplegar1").style.display="block"; 
-  document.getElementById("mas1").style.display="none";
-  break;
-  case "menos":
-  document.getElementById("desplegar").style.display="none";
-  document.getElementById("mas").style.display="inline";
-  break;
-  case "menos3":
-  document.getElementById("desplegar3").style.display="none";
-  document.getElementById("mas3").style.display="inline";
-  break;
-  case "menos2":
-  document.getElementById("desplegar2").style.display="none";
-  document.getElementById("mas2").style.display="inline";
-  break;
-  case "menos1":
-  document.getElementById("desplegar1").style.display="none";
-  document.getElementById("mas1").style.display="inline";
-  break;
-  default:
-  document.getElementById("desplegar").style.display="none";
-  document.getElementById("mas").style.display="inline";
-  break;
-}
 
+function vermas(id){
+boton = id.substring(0,3);
+if (boton=="mas"){
+nro_boton = id.substring(3,4);
+desplegar = ("desplegar" + nro_boton)
+mas = ("mas" + nro_boton)
+
+document.getElementById("desplegar" + nro_boton).style.display="block";  
+document.getElementById("mas").style.display="none";
+}
+else{
+nro_boton = id.substring(5,6);
+desplegar = (desplegar + nro_boton)
+menos = ("menos" + nro_boton)
+
+document.getElementById("desplegar").style.display="none";
+document.getElementById("mas").style.display="inline";
+}
 }
